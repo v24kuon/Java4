dcfd0b58288a7e8fc48bf6322dbbc8f199f5309d
v24kuon <v24kuong@gmail.com>
2020-12-17 19:55:48 +0900

    Java4

---
+++ src/Chapter04.java
@@ -0,0 +1,11 @@
+public class Chapter04 {
+	public static void main(String[] args) {
+		String Str1;
+		Str1 = "Gawl Gura";
+		System.out.println(Str1);
+		Str1 = "しけ村";
+		System.out.println(Str1);
+		final int LUCKY_NUMBER = 3;
+		System.out.println(LUCKY_NUMBER);
+	}
+}
